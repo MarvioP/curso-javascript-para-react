b2f1856003d2b87c87f100c3985fc675940c9612
Marvio Pinheiro <marviop21@gmail.com>
2022-09-17 23:10:03 -0300

    introduce new features

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-
-import CardGame from "./src/components/CardGame";
+import BoardGame from "./src/objects/BoardGame";
 
 const $root = document.querySelector("#app");
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(3) 
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
 
